df27c5209a7463668872308238b18e9d9c360e62
Edmund Blomley <edmund.blomley@kit.edu>
2026-04-14 16:49:57 +0200

    Add initial basic example

--- iocs/cfaExampleIOC/iocBoot/iocLinux/st.cmd
+++ iocs/cfaExampleIOC/iocBoot/iocLinux/st.cmd
@@ -1,25 +1,14 @@
 #!../../bin/linux-x86_64/cfaExample
 
-#- SPDX-FileCopyrightText: 2003 Argonne National Laboratory
-#-
-#- SPDX-License-Identifier: EPICS
-
-#- You may have to change cfaExample to something else
-#- everywhere it appears in this file
-
 < envPaths
 
 cd "${TOP}"
 
-## Register all support components
 dbLoadDatabase "dbd/cfaExample.dbd"
 cfaExample_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-#dbLoadRecords("db/cfaExample.db","user=hi0724")
+dbLoadRecords("db/cfaExampleSimple.db","P=CFA:,R=Simple:")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=hi0724"
